8a31648b8f8e1ecfba7815e8a5f683dbd71bd398
lenard123 <lenard.mangayayam@gmail.com>
2023-04-03 03:42:25 +0000

    Setup tailwind

---
+++ lenard/tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.php", "!./node_modules/**"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          lighter: "rgb(var(--color-primary-lighter) / <alpha-value>)",
+          DEFAULT: "rgb(var(--color-primary) / <alpha-value>)",
+          darker: "rgb(var(--color-primary-darker) / <alpha-value>)",
+        },
+        secondary: {
+          lighter: "rgb(var(--color-secondary-lighter) / <alpha-value>)",
+          DEFAULT: "rgb(var(--color-secondary) / <alpha-value>)",
+          darker: "rgb(var(--color-secondary-darker) / <alpha-value>)",
+        },
+      },
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
